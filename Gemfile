c2a4ce870c401e2ea607127e068970a04b8bca50
rickpr <fdisk@fdisk.co>
2014-07-25 12:41:13 -0600

    Prawn addition

--- Gemfile
+++ Gemfile
@@ -38,4 +38,4 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
-gem 'prawn'
+gem 'prawn', '0.13'
--- app/controllers/shrimp_controller.rb
+++ app/controllers/shrimp_controller.rb
@@ -1,7 +1,15 @@
 class ShrimpController < ApplicationController
   def index 
-    Prawn::Document.generate("public/hello.pdf") do
-      text "Hello World!"
+    pdf_file_paths = ["1.pdf", "2.pdf"]
+      Prawn::Document.generate("result.pdf", {:page_size => 'A4', :skip_page_creation => true}) do |pdf|
+        pdf_file_paths.each do |pdf_file|
+          if File.exists?(pdf_file)
+            pdf_temp_nb_pages = Prawn::Document.new(:template => pdf_file).page_count
+            (1..pdf_temp_nb_pages).each do |i|
+            pdf.start_new_page(:template => pdf_file, :template_page => i)
+          end
+        end
+      end
     end
   end
 end
